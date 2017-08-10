a6c50f0f7d5e1219f95efa531313de735f1ab9ca
Adam Neef <adneef@gmail.com>
2017-08-10 17:05:18 -0600

    part 1 complete

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+//create vars for btns
+  let stopBtn = document.getElementById('stopButton')
+  let slowBtn = document.getElementById('slowButton')
+  let goBtn = document.getElementById('goButton')
+
+//create vars for lights themselves
+  let stopLight = document.querySelector('#stopLight')
+  let slowLight = document.querySelector('#slowLight')
+  let goLight = document.querySelector('#goLight')
+
+  //btn and light control - on and off - lights are independent
+  stopBtn.addEventListener("click", function(){
+    stopLight.classList.toggle('stop')
+  })
+  slowBtn.addEventListener("click", function(){
+    slowLight.classList.toggle('slow')
+  })
+  goBtn.addEventListener("click", function(){
+    goLight.classList.toggle('go')
+  })
 })();
